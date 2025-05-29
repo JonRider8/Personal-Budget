636797780473a1f851f488a4567da16a4d33ce85
Jonathan Rider <jonprider8@gmail.com>
2025-05-29 19:45:23 -0400

    Created basic functionality to envelope

--- server/envelope.js
+++ server/envelope.js
@@ -1,2 +1,7 @@
-const express = require('express');
-const app = express();
+const envelopeRouter = require('express').Router();
+
+module.exports = envelopeRouter;
+
+envelopeRouter.get('/', (req, res) => {
+  res.send('Welcome to the Envelope API');
+});
