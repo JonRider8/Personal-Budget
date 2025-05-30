e47e7bd69d89bc7594e298e294862f5b2463f6e2
Jonathan Rider <jonprider8@gmail.com>
2025-05-30 00:18:32 -0400

    Update on frontend

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('browser'));
 
 const apiRouter = require('./server/api');
 app.use('/api', apiRouter);
+
+app.get('/', (req, res) => {
+    res.redirect('/browser/index.html');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
